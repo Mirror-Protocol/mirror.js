7d3698856f56bf620d17561d0a68dc29dbc8507b
Paul Kim <paul@terra.money>
2020-11-10 23:18:30 +0900

    fix: rmove unnessary files

--- dist/src/utils/EmptyKey.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { Key } from '@terra-money/terra.js';
-export declare class EmptyKey extends Key {
-    constructor();
-    sign(): Promise<Buffer>;
-}
--- dist/src/utils/EmptyObject.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type EmptyObject = {
-    [_ in any]: never;
-};
